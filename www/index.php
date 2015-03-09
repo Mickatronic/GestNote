d9dafb9c832322c4dd4144984d2e412a93421db6
Mickatronic <honvault.m@gmail.com>
2015-03-09 14:15:27 +0100

    maj
    maj

--- www/index.php
+++ www/index.php
@@ -1,1 +1,25 @@
-qssd
+<html>
+	<head>
+		<?php 
+		include 'includes.html';
+		?>
+	</head>
+	<body>
+		<?php
+			session_start();
+			include 'navbar.php';
+		?>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-2">
+					Menu
+				</div>
+				<div class="col-sm-10">
+					Section
+				</div>
+			</div>
+		</div>
+	</body>
+	<footer>
+	</footer>
+</html>
