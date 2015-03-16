2e616c10576a97905cadb985db0b7b47ed344b01
LNVM <crocc@hotmail.fr>
2015-03-16 15:00:30 +0100

    recup version aide
    recup version aide

--- www/Aide.php
+++ www/Aide.php
@@ -1,33 +1,16 @@
-<?php
-//Page Acceuil
-	session_start();	
-?>
 <html>
-	<head>
+    <head>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 
-		<title>Mon espace</title>
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="bootstrap/js/bootstrap.js"></script>
-		
-		<style>
-		.navbar-brand2
-		{
-			position: absolute;
-			width: 100%;
-			left: 0;
-			text-align: center;
-			margin: auto;
-			margin-top:10px;
-		}
-		</style>
-		
-	</head>
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap/js/bootstrap.js"></script>
+	
+  </head>
   
+<<<<<<< HEAD
 	<body>
 		<nav class="navbar navbar-default">
 			<div class="container-fluid">
@@ -67,10 +50,32 @@
 	</form>
 				<div  class="navbar-form navbar-right">
 					<a href="deconnexion.php" class="btn btn-danger">Déconnexion</a>
+=======
+  <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="Accueil.php">Accueil</a>
+    </div>
+	 <form class="navbar-form navbar-right" role="search">
+	    
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="login">
+		  <input type="text" class="form-control" placeholder="mot de passe">
+        </div>
+        <button type="submit" class="btn btn-info">Connexion</button>
+		<button type="submit" class="btn btn-success">Inscription</button>
+>>>>>>> parent of f8263a7... aide
 		
-				</div>
-
-			</div>
-		</nav>
-	</body>
-</html>
+      </form>
+   </div>
+   </nav>
+	
+	
