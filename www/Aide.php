84a018902c8f7fe52c32b042d81514388f2dadba
LNVM <crocc@hotmail.fr>
2015-03-16 08:24:21 +0100

    Page Aide.php
    Page Aide.php

--- www/Aide.php
+++ www/Aide.php
@@ -1,1 +1,39 @@
-<
+<html>
+    <head>
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="bootstrap/js/bootstrap.js"></script>
+	
+  </head>
+  
+  <body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="Accueil.php">Accueil</a>
+    </div>
+	 <form class="navbar-form navbar-right" role="search">
+	    
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="login">
+		  <input type="text" class="form-control" placeholder="mot de passe">
+        </div>
+        <button type="submit" class="btn btn-info">Connexion</button>
+		<button type="submit" class="btn btn-success">Inscription</button>
+		
+      </form>
+   </div>
+   </nav>
+	
+	
