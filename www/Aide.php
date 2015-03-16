afa044f0037301c3a88808d0a2977e311eaf9bbc
Mickatronic <honvault.m@gmail.com>
2015-03-16 15:26:30 +0100

    Micka Page Aide
    Micka Page Aide

--- www/Aide.php
+++ www/Aide.php
@@ -1,20 +1,20 @@
 <html>
     <head>
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="bootstrap/js/bootstrap.js"></script>
-	
-  </head>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="bootstrap/js/bootstrap.js"></script>
+
+	</head>
   
-<<<<<<< HEAD
+
 	<body>
 		<nav class="navbar navbar-default">
 			<div class="container-fluid">
-			<!-- Brand and toggle get grouped for better mobile display -->
+				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 						<span class="sr-only">Toggle navigation</span>
@@ -29,53 +29,20 @@
 					<a href="MonProfil.php" class="btn btn-info">Mon Profil</a>
 				</div>
 
-				<div href="MonProfil.php" class="navbar-brand2">Aide</div>
+				<div class="navbar-brand" style="margin-left:200px">Aidez la siouplez !</div>
+				<form action="Connexion.php" method="GET" class="navbar-form navbar-right" role="search">
+					<div class="form-group">
+						<input name="login" type="text" class="form-control" placeholder="login"/>
+						<input id="Mdp" name="Mdp" type="password" placeholder="mot de passe" class="form-control input-md"/>
+					</div>
+					<input type="submit" class="btn-sd btn btn-info" value="connexion"/>
+					<a href="inscription.php" class="btn btn-sd btn-success">Inscription</a>
 
+				</form>
+			</div>
+		</nav>
+	</body>
+</html>
 
-				<div href="MonProfil.php" class="navbar-brand2">Aide</div>
-				<div  class="navbar-form navbar-right">
-					<a href="deconnexion.php" class="btn btn-danger">Deconnexion</a>
-		
-				</div>
-			<div href="MonProfil.php" class="navbar-brand2">Aidez la siouplez !</div>
-<form action="Connexion.php" method="GET" class="navbar-form navbar-right" role="search">
-		<div class="form-group">
-		  <input name="login" type="text" class="form-control" placeholder="login">
-		      <input id="Mdp" name="Mdp" type="password" placeholder="mot de passe" class="form-control input-md">
-		 
-		</div>
-		<input type="submit" class="btn-sd btn btn-info" value="connexion"/>
-		<a href="inscription.php" class="btn btn-sd btn-success">Inscription</a>
-
-	</form>
-				<div  class="navbar-form navbar-right">
-					<a href="deconnexion.php" class="btn btn-danger">Déconnexion</a>
-=======
-  <body>
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="Accueil.php">Accueil</a>
-    </div>
-	 <form class="navbar-form navbar-right" role="search">
-	    
-        <div class="form-group">
-          <input type="text" class="form-control" placeholder="login">
-		  <input type="text" class="form-control" placeholder="mot de passe">
-        </div>
-        <button type="submit" class="btn btn-info">Connexion</button>
-		<button type="submit" class="btn btn-success">Inscription</button>
->>>>>>> parent of f8263a7... aide
-		
-      </form>
-   </div>
-   </nav>
 	
 	
