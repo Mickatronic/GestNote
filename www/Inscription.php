d70ac5c85bdf9545fdb97c2154a8ed9a3f7658c7
mfgd <mfdomba@gmail.com>
2015-03-23 14:05:53 +0100

    Inscription
    2

--- www/Inscription.php
+++ www/Inscription.php
@@ -19,79 +19,100 @@
 		<div class="">
 			<div class="row ">
 				<div class="col-sm-offset-4 col-sm-5 jumbotron">
-					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Nom: </label>
-							<div class="input-group">
-								<span class="input-group-addon" id="start-date"><span class="glyphicon glyphicon-user"></span></span>
-								<input type="text" class="form-control" aria-describedby="start-date">
+					<form action="Inscription-validation.php" method="GET"> 
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Nom: </label>
+								<div class="input-group">
+									<span class="input-group-addon" id="start-date"><span class="glyphicon glyphicon-user"></span></span>
+									<input type="text" name="Nom" class="form-control" aria-describedby="start-date">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Prénom: </label>
-							<div class="input-group">
-							
-								<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-								<input type="text" aria-describedby="start-date" class="form-control">
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Prénom: </label>
+								<div class="input-group">
+								
+									<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+									<input type="text" name="Prenom" aria-describedby="start-date" class="form-control">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Login: </label>
-							<div class="input-group">
-							
-								<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
-								<input type="text" aria-describedby="start-date" class="form-control">
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Login: </label>
+								<div class="input-group">
+								
+									<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
+									<input type="text" name="Login" aria-describedby="start-date" class="form-control">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Date: </label>
-							<div class="input-group">
-							
-								<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
-								<input type="text" aria-describedby="start-date" class="form-control" placeholder="jj/mm/aaaa">
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Validation: </label>
+								<div class="input-group">
+								
+									<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
+									<input type="text" name="Validation" aria-describedby="start-date" class="form-control">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-						  <label class="control-label" for="selectbasic">Classe: </label>
-						  <div class="">
-							<select id="selectbasic" name="selectbasic" class="form-control">
-							  <option value="1">1</option>
-							  <option value="2">2</option>
-							  <option value="3">3</option>
-							  <option value="4">4</option>
-							  <option value="5">5</option>
-							</select>
-						  </div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-							<label class="control-label">Adresse Mail</label>
-							<div class="input-group">
-							
-								<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
-								<input type="text" aria-describedby="start-date" class="form-control">
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Date: </label>
+								<div class="input-group">
+								
+									<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-star"></span></span>
+									<input type="text" name="Date" aria-describedby="start-date" class="form-control" placeholder="jj/mm/aaaa">
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="row">
-						<div class="form-group">
-						  <div class="col-md-12">
-							<a href="MonEspace.php" name="button1id" class="btn btn-success">Inscription</a>
-							<a href="index.php"  name="button2id" class="btn btn-danger">Annulation</a>			
-						  </div>  
+						<div class="row">
+							<div class="form-group">
+							  <label class="control-label" for="selectbasic">Classe: </label>
+							  <div class="">
+								<select id="selectbasic" name="selectbasic" class="form-control">
+								  <option value="1">1</option>
+								  <option value="2">2</option>
+								  <option value="3">3</option>
+								  <option value="4">4</option>
+								  <option value="5">5</option>
+								</select>
+							  </div>
+							</div>
+						<div class="row">
+							<div class="form-group">
+							  <label class="control-label" for="selectbasic">Type: </label>
+							  <div class="">
+								<select id="selectbasic" name="selectbasic" class="form-control">
+								  <option value="1">Professeur</option>
+								  <option value="2">Eleve</option>
+								</select>
+							  </div>
+							</div>
+						<div class="row">
+							<div class="form-group">
+								<label class="control-label">Adresse Mail</label>
+								<div class="input-group">
+								
+									<span id="start-date" class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span></span>
+									<input type="text" name="Email" aria-describedby="start-date" class="form-control">
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="form-group">
+							  <div class="col-md-12">
+							  <input type="submit" value="Inscription" class="btn btn-success"/>
+								<a href="index.php"  name="button2id" class="btn btn-danger">Annulation</a>			
+							  </div>  
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div> <!--Juimbo-->
 	</div>
   </body>
