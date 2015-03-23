8982bf25aee2c48dac8c8bbaa1cf6ad7e3b699d6
LNVM <crocc@hotmail.fr>
2015-03-23 13:13:51 +0100

    modif vite fait
    modif vite fait

--- www/CreerNotes.php
+++ www/CreerNotes.php
@@ -15,34 +15,44 @@
   <?php
 	include 'navbar.php';
   ?>
-<div class="control-group">
-  <label class="control-label" for="selectbasic">Classe </label>		
-	<div class="controls">
-		<select id="selectbasic" name="selectbasic" class="input-xxlarge">
-		  <option>Seconde</option>
-		  <option>Seconde pro</option>
-		  <option>------------------------------------</option>
-		  <option>Général :</option>
-		  <option>Série S</option>
-		  <option>Série ES</option>
-		  <option>Série L</option>
-		  <option>-------------------------------------</option>
-		  <option>Technologique :</option>
-		  <option>STMG</option>
-		  <option>ST2A</option>
-		  <option>ST2S</option>
-		  <option>ST2D</option>
-		  <option>STL</option>
-		  <option>STAV</option>
-		  <option>TMD</option>
-		  <option>Hôtellerie</option>
-		  <option>-------------------------------------</option>
-		  <option>Professionnel</option>
-		  <option>Gestion Administration</option>
-		  <option>-------------------------------------</option>
-		  <option>Autre</option>
-		</select>
+  
+	<div class="container">
+	  <div class="jumbotron">
+		<center><h3>Ajout des notes</h3></center>      
+		<p></p>      
+	  </div> 
 	</div>
-</div>
-	
-	
+	<div class="container">
+	  <div class="jumbotron">
+		<div class="control-group">
+		  <label class="control-label" for="selectbasic">Classe </label>		
+			<div class="controls">
+				<select id="selectbasic" name="selectbasic" class="input-xxlarge">
+				  <option>Seconde</option>
+				  <option>Seconde pro</option>
+				  <option>------------------------------------</option>
+				  <option>Général :</option>
+				  <option>Série S</option>
+				  <option>Série ES</option>
+				  <option>Série L</option>
+				  <option>-------------------------------------</option>
+				  <option>Technologique :</option>
+				  <option>STMG</option>
+				  <option>ST2A</option>
+				  <option>ST2S</option>
+				  <option>ST2D</option>
+				  <option>STL</option>
+				  <option>STAV</option>
+				  <option>TMD</option>
+				  <option>Hôtellerie</option>
+				  <option>-------------------------------------</option>
+				  <option>Professionnel:</option>
+				  <option>Gestion Administration</option>
+				  <option>-------------------------------------</option>
+				  <option>Autre</option>
+				</select>
+			</div>
+		</div>
+		
+	  </div>		
+	</div>
